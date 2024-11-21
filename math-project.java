d851357e0f863fd1a620eea1360134079f303afc
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-11-21 15:27:15 +0000

    Create math-project.java

---
+++ math-project.java
@@ -0,0 +1,8 @@
+System.out.println(2+2); // addition two numbers 
+System.out.println(4*2); // multiply two numbers
+System.out.println(5-3); // subtraction two numbers 
+System.out.println(50/2); // divide two numbers 
+
+int MyPecentage;
+MyPecentage = 10/100 * 500;
+System.out.println(MyPecentage)
