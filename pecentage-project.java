1a787c18788c1ceb04baf34274299edf8efb6287
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-11-21 17:07:07 +0000

    Update pecentage-project.java

--- pecentage-project.java
+++ pecentage-project.java
@@ -1,5 +1,10 @@
+public class Main { 
+  public static void main(String[] args) {
 int x = 10; 
 int y = 100;
 float z = 10.00; 
+int MyPecentageOne;
 MyPecentageOne = z / y * z; 
-System.out.println(myPecentageOne)
+System.out.println(MyPecentageOne); 
+  }
+}
